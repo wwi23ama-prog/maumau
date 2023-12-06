fdda642df754305ad6ffd2b18d81a281cc55c531
Reiner Hüchting <dev@reinerhuechting.de>
2023-12-06 11:26:42 +0100

    Picture-Methode für Card hinzugefügt.

--- maumau/card.go
+++ maumau/card.go
@@ -1,5 +1,7 @@
 package maumau
 
+import "fmt"
+
 // Card repräsentiert eine Spielkarte.
 type Card struct {
 	suit Suit
@@ -9,4 +11,31 @@ type Card struct {
 // String gibt eine Karte als String zurück.
 func (c Card) String() string {
 	return string(c.suit) + string(c.rank)
+}
+
+// Picture gibt einen String zurück, der die Karte als
+// Unicode-Grafik darstellt.
+//
+// Beispiel:
+// ┌───────┐
+// │♠7     │
+// │       │
+// │       │
+// │     ♠7│
+// └───────┘
+func (c Card) Picture() string {
+	topline := "┌───────┐"
+	botline := "└───────┘"
+	midline := "│       │"
+
+	symbolleft := fmt.Sprintf("│%s%s     │", c.suit, c.rank)
+	symbolright := fmt.Sprintf("│     %s%s│", c.suit, c.rank)
+
+	// Sonderfall: Bei 10 sind es zwei Zeichen, daher muss die Zeile angepasst werden.
+	if c.rank == Ten {
+		symbolleft = fmt.Sprintf("│%s%s    │", c.suit, c.rank)
+		symbolright = fmt.Sprintf("│    %s%s│", c.suit, c.rank)
+	}
+
+	return fmt.Sprintf("%s\n%s\n%s\n%s\n%s\n%s", topline, symbolleft, midline, midline, symbolright, botline)
 }
--- maumau/card_test.go
+++ maumau/card_test.go
@@ -14,3 +14,31 @@ func ExampleCard_String() {
 	// Output:
 	// ♠7
 }
+
+// ExampleCardPicture zeigt die Verwendung von Card.Picture().
+// Die Funktion erzeugt eine Karte und gibt sie als Unicode-Grafik aus.
+func ExampleCard_Picture() {
+	// Karte erzeugen: Pik 7
+	c := Card{suit: Spades, rank: Seven}
+
+	// Sonderfall: 10 (zwei Zeichen)
+	c2 := Card{suit: Spades, rank: Ten}
+
+	// Karten als Unicode-Grafik ausgeben
+	fmt.Println(c.Picture())
+	fmt.Println(c2.Picture())
+
+	// Output:
+	// ┌───────┐
+	// │♠7     │
+	// │       │
+	// │       │
+	// │     ♠7│
+	// └───────┘
+	// ┌───────┐
+	// │♠10    │
+	// │       │
+	// │       │
+	// │    ♠10│
+	// └───────┘
+}
